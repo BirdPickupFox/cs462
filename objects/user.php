8f195609ba722a3b4629cb1cb8d43d1ab47eea01
Ryan Bird <rlbird22@gmail.com>
2014-04-06 17:05:05 -0600

    User.acceptUser() function created

--- objects/user.php
+++ objects/user.php
@@ -1,4 +1,6 @@
 <?php
+require_once('../objects/trip.php');
+require_once('../objects/notification.php');
 
 class User
 {
@@ -24,5 +26,43 @@ class User
 			return NULL;
 		}
 		return "Error: a user with this email address already exists";
+	}
+	
+	public function acceptUser($riderEmail, $tripId)
+	{
+		global $db;
+		$tempRider = new self($riderEmail, "");
+		$result = $db->querySingle("SELECT * FROM trips WHERE trip_id='$tripId'", TRUE);
+		if($result !== NULL)
+		{
+			$trip = new Trip();
+			$trip->start = $result['departure_date_time'];
+			$trip->end = $result['arrival_date_time'];
+			$trip->origin = $result['origin_loc'];
+			$trip->destination = $result['destination_loc'];
+			$trip->vehicleId = $result['vehicle_id'];
+			$trip->price = $result['total_cost'];
+			$trip->googleCalendarId = $result['google_calendar_id'];
+			$trip->tripId = $tripId;
+			$trip->error = NULL;
+			$trip->addUser($riderEmail, true);
+			
+			//TODO check for overflow of seating
+			$tempRider->sendNotification("You have been accepted!");
+		}
+	}
+	
+	//ie. Send driver that someone has requested to join trip
+	//send rider that driver has accepted their request
+	public function sendNotification($message)
+	{
+		$this->emailNotification($message);
+	}
+	
+	private function emailNotification($message)
+	{
+		$notification = new Notification($this->email, $message);
+		$notification->sendEmail();
+		
 	}
 }
