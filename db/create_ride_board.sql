90e8d19476b4e8447d09f31450fa66cd9bff823a
Nathan Fox <fox016@gmail.com>
2014-04-10 07:42:03 -0600

    fixed spelling

--- db/create_ride_board.sql
+++ db/create_ride_board.sql
@@ -95,7 +95,7 @@ INSERT INTO vehicles (year, make, model, seat_count, description, owner)
 INSERT INTO trips (origin_loc, destination_loc, departure_date_time, arrival_date_time, vehicle_id, total_cost)
 	VALUES ('Provo, UT, USA', 'San Diego, CA, USA', 1396869000, 1396915800, 1, 200);
 INSERT INTO trips (origin_loc, destination_loc, departure_date_time, arrival_date_time, vehicle_id, total_cost)
-	VALUES ('San Deigo, CA, USA', 'Provo, UT, USA', 1397133600, 1397180400, 1, 200);
+	VALUES ('San Diego, CA, USA', 'Provo, UT, USA', 1397133600, 1397180400, 1, 200);
 
 INSERT INTO trip_users VALUES ('a@a.com', 1, 1);
 INSERT INTO trip_users VALUES ('b@b.com', 1, 0);
